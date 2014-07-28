01e55e587e50ce1830eeec86fdef1d0f33f8a1a2
GamingTurtle <gadget.guy.mp@gmail.com>
2014-07-28 16:15:27 +0100

    Fix bug on startserver.py

--- StartServer.py
+++ StartServer.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def my_form():
+    return render_template("my-form.html")
 
-if __name__ == "__main__":
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == '__main__':
+    app.debug = True
     app.run()
