c7361cf4fd100783635b7c25440048a7325e9841
GamingTurtle <gadget.guy.mp@gmail.com>
2014-07-28 15:26:04 +0100

    Fix the Beetles and bugs

--- Test.py
+++ Test.py
@@ -2,4 +2,4 @@ import urllib, json
 url = "http://api.openweathermap.org/data/2.5/weather?q=London"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-print(data['weather']['description'])
+print(data['weather'][0]['description'])
