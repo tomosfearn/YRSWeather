3f10f06ec5b2043f4e7141e84c59fe55f983d63b
gamingturtle <marley@marleyplant.co.uk>
2014-07-30 07:50:20 +0100

    Added comments and tested using BBC weather and the api works :)

--- StartServer.py
+++ StartServer.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template("my-form.html")
+    return render_template("my-form.html") #Set render template
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -17,21 +17,21 @@ def my_form_post():
 	print("Do nothing")
     elif request.form['scale'] == "celcius":
 	openwindow_at = openwindow_at + 273 #celcius to kelvin
-    elif request.form['scale'] == "fah":
+    elif request.form['scale'] == "fah": #Fah to kelvin
 	openwindow_at = (openwindow_at + 459.67) * 5 / 9  #F to kelvin
-    text = request.form['text']
+    text = request.form['text'] #Get info from First page
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + text #Download the json
     response = urllib.urlopen(url) #Download Json
     data = json.loads(response.read()) #Parse json
-    print("Current Weather in " + text + " " + data['weather'][0]['description'])
-    print(data['main']['temp'])
-    if data['weather'][0]['description'].find("rain") >= 0:
-	return "Shutting your window"
+    print("Current Weather in " + text + " " + data['weather'][0]['description']) #Debug infomation
+    print("Temp: " + str(data['main']['temp'])) #Print temp 
+    if data['weather'][0]['description'].find("rain") >= 0: #Check The weather
+	return "Shutting your window" #Send to page
 	#Close the window(Tom's job)
     else:
 	if data['main']['temp'] >= openwindow_at:
-		return "Opening your window"
+		return "Opening your window" #Send to page
     		#open the window (Tom's job)
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True #Uncomment to enable debugging
     app.run()
