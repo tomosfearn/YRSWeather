b33a9ef7be2c268123c82a3a07d96ed806608a6a
gamingturtle <gadget.guy.mp@gmail.com>
2014-07-30 07:29:10 +0100

    Fixed some issues and added comments

--- StartServer.py
+++ StartServer.py
@@ -1,9 +1,9 @@
 #Used for starting the flask server
-from flask import Flask
-from flask import request
-from flask import render_template
-import json, urllib
-import time
+from flask import Flask #Import flask mains
+from flask import request #Import flask requests
+from flask import render_template #Import flask render templates
+import json, urllib #import api modules
+import time #Imporing time in darlek voice
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,27 +12,26 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    openwindow_at = float(request.form['open'])
-    if request.form['scale'] == "kelvin":
+    openwindow_at = float(request.form['open']) #When it reached ? open window
+    if request.form['scale'] == "kelvin": #Change figures
 	print("Do nothing")
     elif request.form['scale'] == "celcius":
 	openwindow_at = openwindow_at + 273 #celcius to kelvin
     elif request.form['scale'] == "fah":
 	openwindow_at = (openwindow_at + 459.67) * 5 / 9  #F to kelvin
     text = request.form['text']
-    url = "http://api.openweathermap.org/data/2.5/weather?q=" + text
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + text #Download the json
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     print("Current Weather in " + text + " " + data['weather'][0]['description'])
     print(data['main']['temp'])
-    while True:
-    	if data['weather'][0]['description'].find("rain") >= 0:
-		return "Shutting your window"
-		#Close the window(Tom's job)
-    	else:
-		if data['main']['temp'] >= openwindow_at:
-			return "Opening your window"
-    			#open the window (Tom's job)
+    if data['weather'][0]['description'].find("rain") >= 0:
+	return "Shutting your window"
+	#Close the window(Tom's job)
+    else:
+	if data['main']['temp'] >= openwindow_at:
+		return "Opening your window"
+    		#open the window (Tom's job)
 if __name__ == '__main__':
     app.debug = True
     app.run()
