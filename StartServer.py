0fe1b11ccbf847ffc1d09610549738c94b8a95b8
gamingturtle <marley@marleyplant.co.uk>
2014-07-30 10:13:23 +0100

    Random Push

--- StartServer.py
+++ StartServer.py
@@ -25,13 +25,15 @@ def my_form_post():
     data = json.loads(response.read()) #Parse json
     print("Current Weather in " + text + " " + data['weather'][0]['description']) #Debug infomation
     print("Temp: " + str(data['main']['temp'])) #Print temp 
-    if data['weather'][0]['description'].find("rain") >= 0: #Check The weather
-	return "Shutting your window" #Send to page
-	#Close the window(Tom's job)
-    else:
-	if data['main']['temp'] >= openwindow_at:
-		return "Opening your window" #Send to page
-    		#open the window (Tom's job)
+    while True:
+    	if data['weather'][0]['description'].find("rain") >= 0: #Check The weather
+		print("Test")
+		return "Shutting your window" #Send to page
+		#Close the window(Tom's job)
+    	else:
+		if data['main']['temp'] >= openwindow_at:
+			return "Opening your window" #Send to page
+    			#open the window (Tom's job)
 if __name__ == '__main__':
-    #app.debug = True #Uncomment to enable debugging
+    app.debug = True #Uncomment to enable debugging
     app.run() #Run the Server
