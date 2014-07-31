ce74b2e24bcf99f631a9ead72d9c50d4c3f2ccb3
gamingturtle <marley@marleyplant.co.uk>
2014-07-31 13:41:31 +0100

    Smileys all around life is amazing when you do awesome code

--- Tests/StartServer.py
+++ Tests/StartServer.py
@@ -13,19 +13,20 @@ def my_form():
 
 @app.route('/', methods=['POST']) #Set the route
 def my_form_post():
+    OldTemp = "Cruddy Python Variable"
     text = request.form['text'] #Get info from First page
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + text #Download the json
+
     while True:
 	response = urllib.urlopen(url) #Download Json
         data = json.loads(response.read()) #Parse json
-
-    	while OldTemp != data['main']['temp'] or OldWeather != data['weather']['description']:
+    	while OldTemp != data['main']['temp'] or OldWeather != data['weather'][0]['description']:
     		response = urllib.urlopen(url) #Download Json
     		data = json.loads(response.read()) #Parse json
     		print("Current Weather in " + text + " " + data['weather'][0]['description']) #Debug infomation
     		print("Temp: " + str(data['main']['temp'])) #Print temp 
 		OldTemp = data['main']['temp']
-		OldWeather = data['weather']['description']
+		OldWeather = data['weather'][0]['description']
 		if data['weather'][0]['description'].find('rain') > 0:
 			WindowControl.Close()
 		elif data['main']['temp'] >= 298.15:
