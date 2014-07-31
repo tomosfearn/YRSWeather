be11789a1446e1039bbb3fd4e51651acea48ceef
gamingturtle <marley@marleyplant.co.uk>
2014-07-31 10:36:49 +0100

    Added a random file for random reasons

--- WindowControl.py
+++ WindowControl.py
@@ -1,5 +1,8 @@
 import os
+from optparse import OptionParser
+parser = OptionParser()
 
+parser.add_option("-O","--open",help='Open or no')
 def Open():
 	os.chdir("/home/marley/YRSWeather/ArduinoCode/Open")
 	os.system("sudo ino upload")
