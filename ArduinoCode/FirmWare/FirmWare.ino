7a1e3e2cb4c70bc982c48efd6f76947e954acae2
gamingturtle <marley@marleyplant.co.uk>
2014-08-01 08:15:36 +0100

    Removed annoying .d files

--- ArduinoCode/FirmWare/FirmWare.ino
+++ ArduinoCode/FirmWare/FirmWare.ino
@@ -4,51 +4,41 @@ const int sPin = 10;
 int sVal = 0;
 int fVal = 140;
 
-
-void setup() {
-  // initialize serial:
-  Serial.begin(9600);
-  // make the pins outputs:
-  pinMode(13, OUTPUT); 
+void setup()
+{
   pinMode(dPin, OUTPUT);
   pinMode(sPin, OUTPUT);
-  //analogWrite(dPin, sVal);
+  analogWrite(dPin, sVal);
 }
 
-void Open(){
+void actuatorForwards()
+{
   digitalWrite(sPin, HIGH);
   analogWrite(dPin, fVal);
   delay(10000);
   analogWrite(dPin, sVal);
-  delay(1000);  
+  delay(1000);
 }
 
-void Close(){
+void actuatorReverse()
+{
   digitalWrite(sPin, LOW);
   analogWrite(dPin, fVal);
   delay(10000);
   analogWrite(dPin, sVal);
-  delay(10000); 
+  delay(10000);
 }
 
-
-void loop() 
+void loop()
 {
   // if there's any serial available, read it:
-  while (Serial.available() > 0) { //Unknown
-    if (Serial.read() == 'Open') {
-      Open();
-    }
-    else if (Serial.read() == 'Close'){
-      Close(); 
-    }
-  }
+ while (Serial.available() > 0) {
+ if (Serial.read() == 'O') {
+ 	actuatorForwards()
+ }
+ else if (Serial.read() == "C") {
+	actuatorReverse()
+ }
+ }
 }
-
-
-
-
-
-
-
 
